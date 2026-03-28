46038dfd8ed87e55f7f7b40138f3e655e83bf822
David Riley <davidwilliamriley@outlook.com>
2026-03-28 16:44:48 +1100

    Add vite.config.js

--- tracer-ui/vite.config.js
+++ tracer-ui/vite.config.js
@@ -22,7 +22,7 @@ export default defineConfig({
     // /api/anything → http://localhost:8000/anything
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'http://127.0.0.1:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
